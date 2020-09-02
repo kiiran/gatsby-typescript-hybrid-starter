0ffc277e6434870a182b5adb5e53b0182b3cefc8
Ciaran Edwards <hello@ciaran.codes>
2020-09-02 10:10:31 +0100

    Add bundle-analyzer plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,10 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: '@bundle-analyzer/gatsby-plugin',
+      options: { token: '3bd6b3ba2aa1e6a32a231d228bcb40c36b236dbe' },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
